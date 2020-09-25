ad3291148b2426a76bf4a0613e52ebb290a570d3
farhaan-ali <fa8g12@gmail.com>
2020-09-25 17:47:46 +0100

    cleanup

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -38,16 +38,22 @@ class Bookmark
 
   def getting_tag_names
     result = DatabaseConnection.query("SELECT * FROM bookmark_tags WHERE bookmark_id = #{@id}")
+    
     bookmark_tags = result.map {|tag| Bookmark_tag.new(tag['bookmark_id'], tag['tag_id'])}
+    
     array_of_tag_ids = []
+    
     bookmark_tags.each do |bookmark_tags|
       array_of_tag_ids << bookmark_tags.tag_id
     end 
+    
     array_of_tag_names = []
+    
     array_of_tag_ids.each do |tag_ids|
       result2 = DatabaseConnection.query("SELECT content FROM tags WHERE id = #{tag_ids}")
       array_of_tag_names << result2[0]['content']
     end 
+    
     array_of_tag_names
   end
 
